7c59e6b82a7c42415b8d4ad8e9a604357921ffb6
Honnah Weber <lakeside486@protonmail.com>
2025-11-28 15:21:47 -0700

    changed the reigist ern login navbar, and main jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { usePage } from "./layout/PageContext";
+// import { usePage } from "./layout/PageContext";
 
 import Register from "./auth/Register";
 import Login from "./auth/Login";
 import ActivitiesPage from "./activities/ActivitiesPage";
 import Error404 from "./Error404.jsx";
+import { Routes, Route } from "react-router-dom"
+import Layout from "./layout/Layout.jsx";
 
 /**
  * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
@@ -11,11 +13,16 @@ import Error404 from "./Error404.jsx";
  * account will be able to upload and manage their own activities.
  */
 export default function App() {
-  const { page } = usePage();
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<ActivitiesPage />}/>
+        <Route path="register" element={<Register />}/>
+        <Route path="login" element={<Login />}/>
+        <Route path="*" element={<Error404 />}/>
+      </Route>
 
-  if (page === "register") return <Register />;
-  if (page === "login") return <Login />;
-  if (page === "activities") return <ActivitiesPage />;
+    </Routes>
+  )
 
-  return <Error404 />;
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,15 +1,35 @@
 import { useAuth } from "../auth/AuthContext";
-import { usePage } from "./PageContext";
+// import { usePage } from "./PageContext";
+import { NavLink } from "react-router";
+
 
 /** Navbar with site navigation links */
 export default function Navbar() {
   const { token, logout } = useAuth();
-  const { setPage } = usePage();
+  // const { setPage } = usePage();
   return (
     <header>
       <p>Fitness Trackr</p>
       <nav>
-        <a onClick={() => setPage("activities")}>Activities</a>
+        <NavLink to="/">Activities</NavLink>
+        {token ? (
+          <a onClick={() => logout()}>Log out</a>
+        ) : (
+          <>
+        <NavLink to="register">Register</NavLink>
+        <NavLink to="login">Login</NavLink>
+        </>
+        ) }
+      </nav>
+    </header>
+  );
+}
+
+     
+
+
+
+        {/* <a onClick={() => setPage("activities")}>Activities</a>
         {token ? (
           <a onClick={() => logout()}>Log out</a>
         ) : (
@@ -17,8 +37,4 @@ export default function Navbar() {
             <a onClick={() => setPage("register")}>Register</a>
             <a onClick={() => setPage("login")}>Login</a>
           </>
-        )}
-      </nav>
-    </header>
-  );
-}
+        )} */}
